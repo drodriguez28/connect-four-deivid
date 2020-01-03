8d34331d86d1f26083ea0d49cf79eb9b101af063
Deivid Rodriguez <rodriguezdeivid17@gmail.com>
2020-01-02 21:33:40 -0500

    configs

--- main.js
+++ main.js
@@ -2,3 +2,43 @@ $ (document).ready(function(){
     // draw grid 
     const connect4 = new Connect4('#connect4')
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(document).ready(function() {
+//     const connect4 = new Connect4('#connect4')
+  
+//     connect4.onPlayerMove = function() {
+//       $('#player').text(connect4.player);
+//     }
+    
+//     $('#restart').click(function() {
+//       connect4.restart();
+//     })
+//   });
